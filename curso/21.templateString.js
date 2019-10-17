0f339de5536e8c190777b244f8c847c29f0e3ed6
Breno Menezes <brenoma@live.com>
2019-10-17 15:00:52 -0300

    Aulas

---
+++ curso/21.templateString.js
@@ -0,0 +1,9 @@
+const nome = 'Breno'
+const concatenacao = 'Olá ' + nome + '!';
+const template = `
+    Olá
+    ${nome}!`
+console.log(concatenacao, template);
+
+// Expressões
+console.log(`1 + 1 = ${1+1}`);
--- curso/22.strings.js
+++ curso/22.strings.js
@@ -9,4 +9,6 @@ console.log(escola.substring(1));
 console.log(escola.substring(0, 3)); // Indice final até a quantidade de elementos.
 
 console.log('Escola '.concat(escola).concat('!'));
-console.log(escola.replace(0, '3'));
+console.log(escola.replace(/\w/g, '3')); // Expressão regular
+
+console.log('Breno,IFCE,Ciência da Computação'.split(','));
