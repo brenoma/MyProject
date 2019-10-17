5b3073bab0ccb381645ee599d071515f4685aa68
Breno Menezes <brenoma@live.com>
2019-10-17 17:30:55 -0300

    Aulas

--- curso/25.nullEUndefined.js
+++ curso/25.nullEUndefined.js
@@ -10,3 +10,18 @@ let d = c;
 
 d++;
 console.log(c, d);
+
+// ------------------------------------
+
+let valor;
+console.log(valor); // Variável nunca foi inicializada
+
+valor = null;
+console.log(valor); // Variável já está com um valor nulo
+// console.log(valor.toString());
+
+const produto = {};
+console.log(produto.preco);
+produto.preco = 3.00;
+
+console.log(produto.preco);
